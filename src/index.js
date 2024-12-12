df4131c9167f0732d00c8d5c1662d195ec108501
y-shihui <y.shihui54@gmail.com>
2024-12-12 17:38:19 +1100

    Added time function

--- src/index.js
+++ src/index.js
@@ -1,1 +1,31 @@
+function updateTime() {
+  let tokyoDateElement = document.querySelector("#tokyo .date");
+  let tokyoTimeElement = document.querySelector("#tokyo .time");
+  let tokyoTimeZone = moment().tz("Asia/Tokyo");
 
+  tokyoDateElement.innerHTML = tokyoTimeZone.format("DD MMMM YYYY");
+  tokyoTimeElement.innerHTML = tokyoTimeZone.format(
+    "hh:mm:ss[<small>]A[</small>]"
+  );
+
+  let parisDateElement = document.querySelector("#paris .date");
+  let parisTimeElement = document.querySelector("#paris .time");
+  let parisTimeZone = moment().tz("Europe/Paris");
+
+  parisDateElement.innerHTML = parisTimeZone.format("DD MMMM YYYY");
+  parisTimeElement.innerHTML = parisTimeZone.format(
+    "hh:mm:ss[<small>]A[</small>]"
+  );
+
+  let melbourneDateElement = document.querySelector("#melbourne .date");
+  let melbourneTimeElement = document.querySelector("#melbourne .time");
+  let melbourneTimeZone = moment().tz("Australia/Melbourne");
+
+  melbourneDateElement.innerHTML = melbourneTimeZone.format("DD MMMM YYYY");
+  melbourneTimeElement.innerHTML = melbourneTimeZone.format(
+    "hh:mm:ss[<small>]A[</small>]"
+  );
+}
+
+updateTime();
+setInterval(updateTime, 1000);
